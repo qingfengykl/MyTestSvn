6e925f4ff69e492c2e115d3d95dca924fe612ce1
qingfengykl <qingfengykl@163.com>
2016-03-31 15:12:47 +0800

    测试忽略文件夹3336687788778

---
+++ app/src/main/java/com/example/kelin/mytestsvn/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kelin.mytestsvn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**
+         *
+         *
+         * 测试添加到git
+         */
+    }
+}
